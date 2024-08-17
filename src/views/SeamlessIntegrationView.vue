ee177904e071ae10705af9d50762ef14cd097dd3
Naufal Hardiansyah <naufal.hardiansyah@binus.edu>
2024-08-17 16:32:27 +0700

    adj: change rms ver

--- src/views/SeamlessIntegrationView.vue
+++ src/views/SeamlessIntegrationView.vue
@@ -1,6 +1,6 @@
 <template>
   <teleport to="head">
-    <component :is="'script'" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js" async />
+    <component :is="'script'" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" async />
     <component :is="'script'"
       src="https://pay.merchant.razer.com/RMS/API/seamless/3.28/js/MOLPay_seamless.deco.js" async />
   </teleport>
@@ -10,6 +10,9 @@
     <Button label="Trigger Razer Merchant Service Seamless" data-toggle="molpayseamless"
       data-mpsmerchantid="SB_dreamztech" data-mpschannel="maybank2u" data-mpsamount="1.20"
       data-mpsorderid="TEST1139669863" data-mpsbill_name="MOLPay Technical" />
+
+      <!-- Button trigger Razer Merchant Services Seamless -->
+<button type="button" id="myPay" class="btn btn-primary btn-lg" data-toggle="molpayseamless" data-mpsmerchantid="molpaymerchant" data-mpschannel="maybank2u" data-mpsamount="1.20" data-mpsorderid="TEST1139669863" data-mpsbill_name="MOLPay Technical" >Pay by Maybank2u</button>
   </div>
 
 </template>
