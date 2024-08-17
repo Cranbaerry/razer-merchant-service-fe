97f2afedac11562534b398ec7e419a64eb17ceec
Naufal Hardiansyah <naufal.hardiansyah@binus.edu>
2024-08-17 17:21:24 +0700

    remove unused btn

--- src/views/SeamlessIntegrationView.vue
+++ src/views/SeamlessIntegrationView.vue
@@ -30,11 +30,6 @@ export default {
     <Button label="Trigger Razer Merchant Service Seamless" data-toggle="molpayseamless"
       data-mpsmerchantid="SB_dreamztech" data-mpschannel="maybank2u" data-mpsamount="1.20"
       data-mpsorderid="TEST1139669863" data-mpsbill_name="MOLPay Technical" />
-
-    <!-- Button trigger Razer Merchant Services Seamless -->
-    <button type="button" id="myPay" class="btn btn-primary btn-lg" data-toggle="molpayseamless"
-      data-mpsmerchantid="SB_dreamztech" data-mpschannel="maybank2u" data-mpsamount="1.20"
-      data-mpsorderid="TEST1139669863" data-mpsbill_name="MOLPay Technical">Pay by Maybank2u</button>
   </div>
 
 </template>
