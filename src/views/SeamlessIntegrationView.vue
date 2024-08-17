cdcf867cd1e57fdf48a56a4e085b997a5791cf7e
Naufal Hardiansyah <naufal.hardiansyah@binus.edu>
2024-08-17 16:00:00 +0700

    first commit

---
+++ src/views/SeamlessIntegrationView.vue
@@ -0,0 +1,24 @@
+<template>
+  <teleport to="head">
+    <component :is="'script'" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js" async />
+    <component :is="'script'"
+      src="https://sandbox.merchant.razer.com/RMS/API/seamless/latest/js/MOLPay_seamless.deco.js" async />
+  </teleport>
+
+  <div class="about">
+    <!-- Button trigger Razer Merchant Services Seamless -->
+    <Button label="Trigger Razer Merchant Service Seamless" data-toggle="molpayseamless"
+      data-mpsmerchantid="SB_dreamztech" data-mpschannel="maybank2u" data-mpsamount="1.20"
+      data-mpsorderid="TEST1139669863" data-mpsbill_name="MOLPay Technical" />
+  </div>
+
+</template>
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
